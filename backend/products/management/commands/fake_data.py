86968ea193d3038789931245efecdcd70988f2af
Badr <benhilalbadr03@gmail.com>
2026-04-01 12:01:57 +0100

    first commit

---
+++ backend/products/management/commands/fake_data.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+from products.models import Category, Product
+from faker import Faker
+import random
+
+class Command(BaseCommand):
+    help = 'Generate fake data for products'
+
+    def handle(self, *args, **options):
+        faker = Faker('fr_FR')
+
+        # Create 5 categories
+        categories = []
+        for i in range(5):
+            name = faker.word().capitalize()
+            slug = faker.slug(name)
+            categorie = Category.objects.create(name=name, slug=slug)
+            categories.append(categorie)
+            self.stdout.write(f'Created category with succes: {name}')
+
+        # Create products
+        for i in range(8):
+            Product.objects.create(
+                name=faker.sentence(nb_words=4).replace('.',''),
+                description=faker.text(max_nb_chars=450),
+                category=random.choice(categories),
+                price=faker.random_number(digits=4),
+                stock=faker.random_int(0, 100)
+            )
+
+        self.stdout.write(f'Product : {i+1} created with succes')
